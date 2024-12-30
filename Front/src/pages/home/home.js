1dd41443df40866867491297763933d1548bf7f2
Vini-Araujoo <241025425@aluno.unb.br>
2024-12-30 20:22:25 -0300

    Animação links do header funcionando

--- Front/src/pages/home/home.js
+++ Front/src/pages/home/home.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+//--------------------Header------------------------
 
 export const Nav = styled.nav`
     display: flex;
@@ -16,4 +17,24 @@ export const NavLinks = styled.a`
     padding-left: 20px;
     color: #fff;
     text-decoration: none;
+    position: relative;
+
+    &::after {
+        content: '';
+        display: block;
+        width: 100%;
+        height: 3px;
+        background-color:rgb(18, 154, 239);
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+        transform: scaleX(0);
+        transform-origin: left;
+        transition: transform 0.3s ease;    
+    }
+
+    &:hover::after {
+        transform: scaleX(1); 
+    }
 `
+//------------------------Corpo----------------------------
