86c6464185f1279e87575bac7dc65d3cf02e4ff3
Vini-Araujoo <241025425@aluno.unb.br>
2024-12-30 20:06:17 -0300

    Nav feita sem componentização

---
+++ Front/src/pages/home/home.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`
+
+export const NavBar = styled.ul`
+    list-style: none;
+    display: flex;
+    gap: 40px;
+`
+
+export const NavLinks = styled.a`
+    padding-left: 20px;
+    color: #fff;
+    text-decoration: none;
+`
--- Front/src/pages/home/index.jsx
+++ Front/src/pages/home/index.jsx
@@ -1,4 +1,7 @@
-import './style.css'
+import api from '../../services/api'
+import { useState } from 'react';
+import logo from '../../assets/imagens/logo.svg'
+import { NavBar, NavLinks, Nav } from './home';
 
 
 //essa é a pagina raiz do nosso site
@@ -7,12 +10,22 @@ function Home(){
 
 
     return(
-
         <header>
-        <h1>Pagina de inicio</h1>
-
-        <a href="http://localhost:5173/cadastro">cadastre-se</a>
-
+            <Nav>
+                <div className="logo">
+                    <a href="http://localhost:5173/">
+                        <img src={logo} alt="logo" />
+                    </a>                    
+                </div>
+                <div className="links">
+                    <NavBar>
+                        <li><NavLinks href="http://localhost:5173/">Home</NavLinks></li>
+                        <li><NavLinks href="http://localhost:5173/">Sobre</NavLinks></li>
+                        <li><NavLinks href="http://localhost:5173/login">Entre</NavLinks></li>
+                        <li><NavLinks href="http://localhost:5173/">Perfil</NavLinks></li>
+                    </NavBar>
+                </div>
+            </Nav>
         </header>
     )
 
