2b311d4b0e4007200347b00adb8dcb88a8272d26
Vini-Araujoo <241025425@aluno.unb.br>
2024-12-30 21:29:27 -0300

    Main da Home concluído

--- Front/src/pages/home/home.js
+++ Front/src/pages/home/home.js
@@ -5,13 +5,13 @@ export const Nav = styled.nav`
     display: flex;
     align-items: center;
     justify-content: space-around;
-`
+`;
 
 export const NavBar = styled.ul`
     list-style: none;
     display: flex;
     gap: 40px;
-`
+`;
 
 export const NavLinks = styled.a`
     padding-left: 20px;
@@ -36,5 +36,54 @@ export const NavLinks = styled.a`
     &:hover::after {
         transform: scaleX(1); 
     }
-`
+`;
 //------------------------Corpo----------------------------
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* Centraliza o botão horizontalmente */
+    justify-content: space-between;
+    width: 100%;
+    min-height: 100vh; /* Preenche a altura da viewport */
+    padding: 100px;
+    margin-top: 3vh;
+`;
+
+export const Azul = styled.span`
+    color: #00D0FF;
+`;
+
+export const TextoEsquerda = styled.div`
+    color: #fff;
+    flex: 1;
+    text-align: left;
+    width: 75%; 
+    font-size: 19px;
+`;
+
+export const TextoDireita = styled.div`
+    color: #fff;
+    flex: 1;
+    text-align: right;
+    width: 75%;
+    font-size: 19px;
+`;
+
+export const BemVindo = styled.span`
+    color: #00D0FF;
+    font-size: 25px;    
+`;
+
+export const Botao = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: blue;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+
+
+`;
