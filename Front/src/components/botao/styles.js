dc2d06db21fc5b846ffb4c0550aeb09deab7a157
Vini-Araujoo <241025425@aluno.unb.br>
2025-01-01 20:20:56 -0300

    Foi adicionado hover ao botão!

--- Front/src/components/botao/styles.js
+++ Front/src/components/botao/styles.js
@@ -11,6 +11,13 @@ export const Botao = styled.button`
     justify-content: center;
     align-items: center;
     border: 1px solid transparent;
-    border-radius: 3rem;
     border-image: linear-gradient(to left, rgba(4, 11, 16, 1), rgba(2, 188, 255, 1))1;
+    transition: all 0.3s ease; /* Transição suave para o hover */
+
+    &:hover {
+        background-color: rgba(42, 131, 163, 0.2); /* Cor de fundo ao passar o mouse */
+        border-color: rgba(2, 188, 255, 1); /* Alterar a cor da borda */
+        transform: scale(1.05); /* Aumenta ligeiramente o tamanho do botão */
+    }
+
 `;
