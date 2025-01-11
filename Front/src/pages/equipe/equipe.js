df28522c73cce0d6e4ffb578edba09fa81ee0cc1
Vini-Araujoo <241025425@aluno.unb.br>
2025-01-11 19:07:13 -0300

    Página da equipe concluída

--- Front/src/pages/equipe/equipe.js
+++ Front/src/pages/equipe/equipe.js
@@ -69,14 +69,21 @@ export const ImagensP2 = styled.div`
 `
 //--------Equipe---------
 export const Imagem = styled.div`
-    width: 17vw;
-    height: 36vh;
+    width: 10vw;
+    height: 21vh;
     border: 3px solid transparent;
     border-image: linear-gradient(to top, rgba(4, 11, 16, 1), rgba(2, 188, 255, 1))10;
-    border-radius: 1rem;
     display: flex;
     align-items: center;
     justify-content: center;
+
+    overflow: hidden; /* Evita que a imagem extrapole os limites da div */
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+
+    &:hover {
+        transform: scale(1.1); /* Aumenta o tamanho da imagem ao passar o mouse */
+        box-shadow: 0px 4px 15px rgba(2, 188, 255, 0.5); /* Adiciona um leve brilho */
+    }
 
     img {
         max-width: 100%;
@@ -113,4 +120,26 @@ export const L3 = styled.div`
     background-image: linear-gradient(to left, rgba(9, 51, 61, 1), rgba(2, 188, 255, 1));
 `
 
+//----------Caixa de texto-------------
 
+export const Caixa = styled.div`
+    width: 27vw;
+    height: 23vh;
+    border-radius: 1rem;
+    margin-top: 100px;
+    background-image: linear-gradient(to top, rgba(2, 16, 22, 1), rgba(2, 16, 22, 1));
+    border: 1px solid transparent;
+    border-image: linear-gradient(to left, rgba(4, 11, 16, 1), rgba(2, 188, 255, 1))1;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+
+export const Nome = styled.p`
+    margin-top: 10px;
+    text-align: center;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: bold;
+`;
--- Front/src/pages/equipe/index.jsx
+++ Front/src/pages/equipe/index.jsx
@@ -1,19 +1,19 @@
 import logo from '../../assets/imagens/logo.svg'
-import { Nav, NavBar, NavLinks, Azul, Main, ImagensP1, ImagensP2, Imagem, LinhaCentro, L1, L2, L3} from './equipe';
+import { Nav, NavBar, NavLinks, Azul, Main, ImagensP1, ImagensP2, Imagem, LinhaCentro, L1, L2, L3, Caixa, Nome} from './equipe';
 import vini from '../../assets/imagens/fotoVini.jpg'
 import nicole from '../../assets/imagens/fotoNicole.png'
 import guilherme from '../../assets/imagens/fotoGuilherme.png'
 import bruno from '../../assets/imagens/fotoBruno.png'
 
-function Equipe(){
-    return(
+function Equipe() {
+    return (
         <div>
             <header>
                 <Nav>
                     <div className="logo">
                         <a href="http://localhost:5173/">
                             <img src={logo} alt="logo" />
-                        </a>                    
+                        </a>
                     </div>
                     <div className="links">
                         <NavBar>
@@ -27,14 +27,22 @@ function Equipe(){
             </header>
 
             <Main>
-            <h1>Equipe de colaboradores</h1>
+                <h1>Equipe de colaboradores</h1>
                 <ImagensP1>
-     
-                    <Imagem><img src={vini} alt="Foto Vini" /></Imagem>
-                    <Imagem><img src={nicole} alt="Foto Nicole" /></Imagem>
-                    <Imagem><img src={guilherme} alt="Foto Guilherme" /></Imagem>
-
+                    <div>
+                        <a href="#vini"><Imagem><img src={vini} alt="Foto Vini" /></Imagem></a>
+                        <Nome>Vini</Nome>
+                    </div>
+                    <div>
+                        <a href="#nicole"><Imagem><img src={nicole} alt="Foto Nicole" /></Imagem></a>
+                        <Nome>Nicole</Nome>
+                    </div>
+                    <div>
+                        <a href="#guilherme"><Imagem><img src={guilherme} alt="Foto Guilherme" /></Imagem></a>
+                        <Nome>Guilherme</Nome>
+                    </div>
                 </ImagensP1>
+
 
                 <LinhaCentro>
                     <L1></L1>
@@ -43,14 +51,45 @@ function Equipe(){
                 </LinhaCentro>
 
                 <ImagensP2>
-                    <Imagem><img src={bruno} alt="Foto Bruno" /></Imagem>
-                    <Imagem></Imagem>
-                    <Imagem></Imagem>
+                    <div>
+                        <a href="#bruno"><Imagem><img src={bruno} alt="Foto Bruno" /></Imagem></a>
+                        <Nome>Bruno</Nome>
+                    </div>
+                    <div>
+                        <Imagem></Imagem>
+                        <Nome>Nome 1</Nome>
+                    </div>
+                    <div>
+                        <Imagem></Imagem>
+                        <Nome>Nome 2</Nome>
+                    </div>
                 </ImagensP2>
-            </Main>
 
+                <Caixa id="vini">
+                    <h2><Azul>Vini</Azul></h2>
+                    <p>
+                        Estudante do 2° semestre de Engenharia de Software da Universidade de Brasília.
+                        Gerenciou a equipe, desenvolveu o front-end da plataforma e participou ativamente na prototipação do site.
+                    </p>
+                </Caixa>
+
+                <Caixa id="nicole">
+                    <h2><Azul>Nicole</Azul></h2>
+                    <p>Lorem ipsum dolor sit amet. Aut commodi voluptatem ut asperiores repellendus est perspiciatis.</p>
+                </Caixa>
+
+                <Caixa id="guilherme">
+                    <h2><Azul>Guilherme</Azul></h2>
+                    <p>Lorem ipsum dolor sit amet. Aut commodi voluptatem ut asperiores repellendus est perspiciatis.</p>
+                </Caixa>
+
+                <Caixa id="bruno">
+                    <h2><Azul>Bruno</Azul></h2>
+                    <p>Lorem ipsum dolor sit amet. Aut commodi voluptatem ut asperiores repellendus est perspiciatis.</p>
+                </Caixa>
+            </Main>
         </div>
-    )
+    );
 }
 
 export default Equipe;
