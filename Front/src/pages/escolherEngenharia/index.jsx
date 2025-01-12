1221eb5e9fcbbc4c7e7563da96ef5261ad893bc5
Vini-Araujoo <241025425@aluno.unb.br>
2025-01-11 21:14:35 -0300

    Animação da página de escolha de engenharia pronta

--- Front/src/pages/escolherEngenharia/index.jsx
+++ Front/src/pages/escolherEngenharia/index.jsx
@@ -1,35 +1,66 @@
-import logo from '../../assets/imagens/logo.svg'
-import { Nav, NavBar, NavLinks, Main, Azul} from './escolha';
-import aero from '../../assets/imagens/Aeroespacial.svg'
+import styled, { keyframes } from "styled-components";
+import { Nav, NavBar, NavLinks, Main, Azul } from './escolha';
+import logo from '../../assets/imagens/logo.svg';
+import aero from '../../assets/imagens/Aeroespacial.svg';
 
-function Escolher (){
+// Animação de Zoom
+const zoomIn = keyframes`
+  0% {
+    transform: scale(1);
+    filter: brightness(1);
+  }
+  50% {
+    transform: scale(1.1);
+    filter: brightness(1.2);
+  }
+  100% {
+    transform: scale(1);
+    filter: brightness(1);
+  }
+`;
 
-    return(
-        <div>
-            <header>
-                <Nav>
-                    <div className="logo">
-                        <a href="http://localhost:5173/">
-                            <img src={logo} alt="logo" />
-                        </a>
-                    </div>
-                    <div className="links">
-                        <NavBar>
-                            <li><NavLinks href="http://localhost:5173/">Home</NavLinks></li>
-                            <li><NavLinks href="http://localhost:5173/sobre">Sobre</NavLinks></li>
-                            <li><NavLinks href="http://localhost:5173/login">Entre</NavLinks></li>
-                            <li><NavLinks href="http://localhost:5173/perfil">Perfil</NavLinks></li>
-                        </NavBar>
-                    </div>
-                </Nav>
-            </header>
+// Styled-component para imagem com animação
+export const AnimatedImage = styled.img`
+  width: 200px; /* Ajuste conforme necessário */
+  height: auto;
+  animation: ${zoomIn} 3s ease-in-out infinite;
+  transition: transform 0.3s ease, filter 0.3s ease;
 
-            <Main>
-                <h1>Escolha sua <br /><Azul>Engenharia</Azul></h1>
-                <a href=""><img src={aero} alt="Imagem aeroespacial" /></a>
-            </Main>
-        </div>    
-    )
+  &:hover {
+    transform: scale(1.1); /* Aumenta o tamanho da imagem ao passar o mouse */
+    filter: brightness(1.2); /* Aumenta o brilho */
+  }
+`;
+
+function Escolher() {
+  return (
+    <div>
+      <header>
+        <Nav>
+          <div className="logo">
+            <a href="http://localhost:5173/">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+          <div className="links">
+            <NavBar>
+              <li><NavLinks href="http://localhost:5173/">Home</NavLinks></li>
+              <li><NavLinks href="http://localhost:5173/sobre">Sobre</NavLinks></li>
+              <li><NavLinks href="http://localhost:5173/login">Entre</NavLinks></li>
+              <li><NavLinks href="http://localhost:5173/perfil">Perfil</NavLinks></li>
+            </NavBar>
+          </div>
+        </Nav>
+      </header>
+
+      <Main>
+        <h1>Escolha sua <br /><Azul>Engenharia</Azul></h1>
+        <a href="">
+          <AnimatedImage src={aero} alt="Imagem aeroespacial" />
+        </a>
+      </Main>
+    </div>
+  )
 }
 
 export default Escolher;
