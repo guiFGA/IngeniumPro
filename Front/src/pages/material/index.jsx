7636eed7ad00fe7d07d7b00aaca7190b3d3ec90f
guiFGA <guiazevedoo3008@gmail.com>
2025-01-29 09:56:51 -0300

    dinamicidade dos materiais completa

--- Front/src/pages/material/index.jsx
+++ Front/src/pages/material/index.jsx
@@ -15,16 +15,37 @@ function Material() {
     const { id, tipo } = useParams()
     const [foto1, setFoto1] = useState('')
     const [foto2, setFoto2] = useState('')
+    const [foto3, setFoto3] = useState('')
     const [nome1, setNome1] = useState('')
+    const [nome2, setNome2] = useState('')
+    const [nome3, setNome3] = useState('')
+    const [conteudo1, setConteudo1] = useState('')
+    const [conteudo2, setConteudo2] = useState('')
+    const [conteudo3, setConteudo3] = useState('')
 
     function carregar() {
 
-        console.log({ tipo })
+        
         api
             .post('/mostrarmaterial', {id, tipo})
             .then((response) => {
 
-               
+                //setando as fotos
+                setFoto1(response.data[0].foto)
+                setFoto2(response.data[1].foto)
+                setFoto3(response.data[2].foto)
+                
+                //setando os nomes
+                setNome1(response.data[0].nome)
+                setNome2(response.data[1].nome)
+                setNome3(response.data[2].nome)
+
+                //setando conteudo
+                setConteudo1(response.data[0].conteudo)
+                setConteudo2(response.data[1].conteudo)
+                setConteudo3(response.data[2].conteudo)
+
+                
 
             })
 
@@ -58,32 +79,32 @@ function Material() {
             </header>
 
             <Main>
-                <Titulo>Material X</Titulo>
+                <Titulo>{tipo}</Titulo>
                 <Container>
-                    <Card target="black" href="https://edisciplinas.usp.br/pluginfile.php/8438608/mod_resource/content/1/Fundamentos%20de%20F%C3%ADsica%20-%20Halliday%20-%209%C2%AA%20Edi%C3%A7%C3%A3o%20Vol.1%20Ed.%209.pdf">
+                    <Card target="black" href={conteudo1}>
                         <Cima>
-                            <img src={Halliday} alt="Imagem do livro Halliday" />
+                            <img src={foto1} alt="Imagem do livro Halliday" />
                         </Cima>
                         <Baixo>
-                            <p>Material: <Azul>Halliday</Azul></p>
+                            <p>Material: <Azul>{nome1}</Azul></p>
                         </Baixo>
                     </Card>
 
-                    <Card target="black" href="https://edisciplinas.usp.br/pluginfile.php/8438608/mod_resource/content/1/Fundamentos%20de%20F%C3%ADsica%20-%20Halliday%20-%209%C2%AA%20Edi%C3%A7%C3%A3o%20Vol.1%20Ed.%209.pdf">
+                    <Card target="black" href={conteudo2}>
                         <Cima>
-                            <img src={Halliday} alt="Imagem do livro Halliday" />
+                            <img src={foto2} alt="Imagem do livro Halliday" />
                         </Cima>
                         <Baixo>
-                            <p>Material: <Azul>Halliday</Azul></p>
+                            <p>Material: <Azul>{nome2}</Azul></p>
                         </Baixo>
                     </Card>
 
-                    <Card target="black" href="https://edisciplinas.usp.br/pluginfile.php/8438608/mod_resource/content/1/Fundamentos%20de%20F%C3%ADsica%20-%20Halliday%20-%209%C2%AA%20Edi%C3%A7%C3%A3o%20Vol.1%20Ed.%209.pdf">
+                    <Card target="black" href={conteudo3}>
                         <Cima>
-                            <img src={Halliday} alt="Imagem do livro Halliday" />
+                            <img src={foto3} alt="Imagem do livro Halliday" />
                         </Cima>
                         <Baixo>
-                            <p>Material: <Azul>Halliday</Azul></p>
+                            <p>Material: <Azul>{nome3}</Azul></p>
                         </Baixo>
                     </Card>
                 </Container>
